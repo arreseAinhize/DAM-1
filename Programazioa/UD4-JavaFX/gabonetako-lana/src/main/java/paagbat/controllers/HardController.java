8b51473a9c2117bc9d45f807810f8a0467bce76a
Ainhize Arrese <arrese.ainhize@uni.eus>
2024-12-22 18:04:32 +0100

    2024-12-22

--- Programazioa/UD4-JavaFX/gabonetako-lana/src/main/java/paagbat/controllers/HardController.java
+++ Programazioa/UD4-JavaFX/gabonetako-lana/src/main/java/paagbat/controllers/HardController.java
@@ -40,10 +40,10 @@ public class HardController {
 
     private final String[][] questions = {
         {"Non ospatuko da 2025ko bizkaiko dantzari eguna?", "Zornotza", "Lekeitio", "Sestao", "Basauri", "2","04.jpg","",""},
-        {"Zein izen du hurrengo dantzak? adi musikari", "Makil joko", "Ezpata naguzi", "Txotxongilo", "Ezpata txiki", "2","02.jpg","Musika","ezpataNagusi.mp3"},
+        {"Zein izen du hurrengo dantzak? adi musikari", "Makil joko", "Ezpata nagusi", "Txotxongilo", "Ezpata txiki", "2","02.jpg","Musika","ezpataNagusi.mp3"},
         {"Zein izen du hurrengo dantzak?", "Agintariena", "Agurra", "Aurrezkua", "Zeharkakoa", "1","01.jpg","",""},
         {"Zer tresna erabiltzen dira dantzari-dantzan?", "Ezpatak eta makilak", "Arkuak", "Makilak", "Zestak", "1","05.jpg","",""},
-        {"Zein izen du hurrengo dantzak? adi musikari", "Makil joko", "Ezpata naguzi", "Txotxongilo", "Ezpata txiki", "1","08.jpg","Musika","makil.mp3"},
+        {"Zein izen du hurrengo dantzak? adi musikari", "Makil joko", "Ezpata nagusi", "Txotxongilo", "Ezpata txiki", "1","08.jpg","Musika","makil.mp3"},
         {"Zein izen du hurrengo dantzak?", "Kontrapas", "Sagar dantza", "Zinta dantza", "Kaskarot", "3","03.jpg","",""},
         {"Non egiten dira ohituraz Erregelak dantzak?", "Lekeitio eta Ondarroan", " Bakion eta Mungian", "Gernika eta Garain", "Basauri eta Karrantzan", "3","06.jpg","",""},
         {"Non egiten da ohituraz Karmen eguneko dantza?", "Mungian", "Bakion", "Markina-Xeimein", "Ermun", "3","07.jpg","",""},
